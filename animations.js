981b2e5a494a45c8f436fa27f5142ff95344233d
ChenGP40 <91141755+ChenGP40@users.noreply.github.com>
2025-01-24 06:44:56 -0500

    Add files via upload

--- animations.js
+++ animations.js
@@ -8,13 +8,13 @@ gsap.from(".SolutionArray2", {y:200, opacity:0, duration: 1, ease: "power3.inOut
 gsap.from(".infoBox5", {y:200, duration: 1, scrollTrigger: ".infoBox5", ease: "power3.inOut" });
 gsap.from(".infoBox2", {opacity:0, duration: 1, scrollTrigger: ".infoBox2", ease: "power3.inOut" });
 
-gsap.from(".descriptionFloat", {opacity:0, duration: 1, scrollTrigger: ".descriptionFloat2", ease: "power3.inOut" });
+gsap.from(".descriptionFloat", {opacity:0, duration: 1, scrollTrigger: ".separator", ease: "power3.inOut" });
 gsap.from("#LHimg", {x:-200,opacity:0, duration: 1, scrollTrigger: ".floatBtn2", ease: "power3.In" });
 gsap.from("#RHimg", {x:200,opacity:0, duration: 1, scrollTrigger: ".floatBtn2", ease: "power3.In" });
-gsap.from(".floatBtn2", {x:200,opacity:0, duration: 1, scrollTrigger: ".descriptionFloat2", ease: "power3.In" });
-gsap.from(".floatBtn3", {x:-200,opacity:0, duration: 1, scrollTrigger: ".descriptionFloat2", ease: "power3.In" });
-gsap.from(".descriptionFloat3", {y:200,opacity:0, duration: 1, scrollTrigger: "div.foregroundBox", ease: "power3.inOut" });
-gsap.from(".descriptionFloat4", {y:200,opacity:0, duration: 1, scrollTrigger: "div.foregroundBox", ease: "power3.inOut" });
+gsap.from(".floatBtn2", {x:200,opacity:0, duration: 1, scrollTrigger: "#trigger2", ease: "power3.In" });
+gsap.from(".floatBtn3", {x:-200,opacity:0, duration: 1, scrollTrigger: "#trigger2", ease: "power3.In" });
+gsap.from(".descriptionFloat3", {y:200,opacity:0, duration: 1, scrollTrigger: "#trigger3", ease: "power3.inOut" });
+gsap.from(".descriptionFloat4", {y:200,opacity:0, duration: 1, scrollTrigger: "#trigger3", ease: "power3.inOut" });
 gsap.from(".menuImg", {y:-200,opacity:0,stagger:.75, duration: .5, scrollTrigger: ".infoBlurb", ease: "power3.inOut" });
 
 function modalAnimation(){
